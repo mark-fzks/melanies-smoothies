387ae343cf2d8b9deefd44c4ebaf0a42915a75db
Mark <fazekasmark95@gmail.com>
2024-11-30 19:32:46 +0000

    for each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        st.subheader(f"{fruit_chosen} Nutrient Information")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -49,9 +53,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect ()
         st.success('Your Smoothie is ordered!', icon="✅" )
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
